99d42c3e1ae4708142452636cb1994a896afe37a
Papuszkin <babajaga@tfwno.gf>
2019-10-11 12:06:17 +0200

    Programy z Wykładu II

---
+++ Wyklad1/Wyklad1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Wyklad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var lk = rnd.Next(maxValue: 100);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Podejscie {0}", i);
+                Console.WriteLine("Podaj liczbę: ");
+                if (!int.TryParse(Console.ReadLine(), out int nl))
+                {
+                    continue;
+                }
+
+                if (nl > lk)
+                {
+                    Console.WriteLine("Za dużo");
+                }
+                else if (nl < lk)
+                {
+                    Console.WriteLine("Za mało");
+                }
+                else
+                {
+                    Console.WriteLine("Super man");
+                    break;
+                }
+            }
+        }
+    }
+}
