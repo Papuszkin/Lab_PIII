71a9152a80951b24d1a2ccb8c584629a50defa77
Papuszkin <babajaga@tfwno.gf>
2019-11-15 10:02:51 +0100

    Program z zajęc 6

---
+++ Zajęcia6/Zajęcia6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Zajęcia6
+{
+    class Program
+    {
+        static void OpiszSamochod(Samochod auto)
+        {
+            Console.WriteLine("[" + auto.VIN + "] " + auto.Model + ": " + auto.Przebieg + "km Ilosc drzwi:" + auto.IloscDrzwi + " Waga:" + auto.Waga + "kg");
+        }
+
+        static void Main(string[] args)
+        {
+            Samochod mojsamochod = new Samochod("Multipla", 100000, 4, 700);
+            OpiszSamochod(mojsamochod);
+            //mojsamochod.Waga = 2000;
+            //mojsamochod.Przebieg = 300000;
+            //OpiszSamochod(mojsamochod);
+
+            Samochod s2 = new Samochod("Punto", 150000, 2, 800);
+            s2.ZwiekszDystans(100);
+            Samochod s3 = new Samochod("Maluch", 305000, 4, 600);
+            OpiszSamochod(s2);
+            OpiszSamochod(s3);
+            Console.WriteLine("Wyprodukowano: " + Samochod.IloscModeli);
+            Samochod s4 = new Samochod(4, 700);
+            OpiszSamochod(s4);
+            
+        }
+    }
+}
---
+++ Zajęcia6/Zajęcia6/Samochod.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zajęcia6
+{
+    class Samochod
+    {
+        public static int IloscModeli { get; private set; } = 0;
+
+        public readonly string Model;
+
+        public int VIN { get; set; }
+
+        public void ZwiekszDystans(int dystans)
+        {
+            if (dystans < 0)
+            {
+                throw new Exception("Nie mozna zmiejszac dystansu");
+            }
+            else
+            {
+                Przebieg += dystans;
+            }
+
+        }
+
+        private int _przebieg;
+        public int Przebieg
+        {
+            get
+            {
+                return _przebieg;
+            }
+            set
+            {
+                if (value > _przebieg)
+                {
+                    _przebieg = value;
+                }
+            }
+        }
+
+        public int IloscDrzwi { get; private set; }
+
+        private int _waga;
+        public int Waga
+        {
+            get
+            {
+                return _waga;
+            }
+            set
+            {
+                _waga = value;
+                if (_waga < 0)
+                {
+                    _waga = 0;
+                }
+            }
+        }
+
+
+        public Samochod(string model, int przebieg, int iloscDrzwi, int waga) : this()
+        {
+            Model = model;
+            Przebieg = przebieg;
+            IloscDrzwi = iloscDrzwi;
+            Waga = waga;
+        }
+
+        public Samochod(int id)
+        {
+            // var dane = Baza.GetById(id);
+            // Przebieg = dane.Przebieg;
+        }
+
+        public Samochod(int iloscDrzwi, int waga) : this("Multipla", 0, iloscDrzwi, waga)
+        {
+            // Ponieważ istnieje :this(...) instrukcje poniżej nie są wymagane
+            //Model = "Multipla";
+            //Przebieg = 0;
+            //IloscDrzwi = iloscDrzwi;
+            //Waga = waga;
+            //VIN = IloscModeli++;
+        }
+
+        private Samochod()
+        {
+            VIN += IloscModeli++;
+        }
+    }
+}
